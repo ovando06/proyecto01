2620b4cb31c419a75f7ac1106d307ac2b3149ca2
ovando06 <lunaovando65@gmail.com>
2024-04-10 10:55:39 -0300

    tienda st

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { Vista1Component } from './vista1/vista1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:"inicio", component: InicioComponent
+},
+{
+  path:"galeria", component: Vista1Component
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

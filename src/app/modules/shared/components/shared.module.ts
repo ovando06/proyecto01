127ac57034191af65a355a4c0cc7f577d029261b
ovando06 <lunaovando65@gmail.com>
2024-05-07 10:55:37 -0300

    creacion de navbar y primer cambio de rutas

--- src/app/modules/shared/components/shared.module.ts
+++ src/app/modules/shared/components/shared.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-//importacion de componentes
+//importacion de componentes locales y globales
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 
+//componentes material
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
+//importamos para acceder a todas las rutas
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -13,11 +19,18 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   exports: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
   ]
 })
 export class SharedModule { }

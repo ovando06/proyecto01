4de7937fca40a30fded0fe7cadb90ff402e58bf0
ovando06 <lunaovando65@gmail.com>
2024-06-04 10:50:57 -0300

    inicio sesion

--- src/app/modules/autentificacion/pages/iniciosesion/iniciosesion.component.ts
+++ src/app/modules/autentificacion/pages/iniciosesion/iniciosesion.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
 
 @Component({
   selector: 'app-iniciosesion',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./iniciosesion.component.css']
 })
 export class IniciosesionComponent {
-hide=true;
+  //input de contraseña
+  hide = true;
+
+  public perfil: Usuario[];
+
+  constructor() {
+    this.perfil = [
+      {
+        uid: '',
+        nombre: 'agostina',
+        apellido: 'ovando',
+        email: 'lunaovando@gmail.com',
+        rol: 'administrador',
+        password: '12345'
+      }
+    ]
+  }
+
+  perfiles: Usuario = {
+    uid: '',//--> inicializamos con comillas vacias porque es string
+    nombre: '',
+    apellido: '',
+    email: '',
+    rol: '',
+    password: '',
+  }
+
+  coleccionPerfiles: Usuario[] = [];
+
+  comparacion() {
+    //constante credenciales va a resguardar la informacion que ingrese el usuario
+    const credenciales = {
+      uid: this.perfiles.uid,
+      nombre: this.perfiles.nombre,
+      apellido: this.perfiles.apellido,
+      email: this.perfiles.email,
+      rol: this.perfiles.rol,
+      password: this.perfiles.password,
+
+    }
+    //enviamos la nueva informacion como un nuevo objeto a la collecion de usuarios
+  this.coleccionPerfiles.push(credenciales)
+
+  //mostramos credenciales por consola
+  console.log(credenciales);
+  console.log(this.coleccionPerfiles);
+  }
+
+  
 }

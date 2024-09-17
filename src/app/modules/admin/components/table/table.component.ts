bccce06b2463b55dafc9c1d7d13e7850d5ab4f10
ovando06 <lunaovando65@gmail.com>
2024-09-17 11:05:41 -0300

    modificar funciones de 'mostrarEditar', 'editarProducto' y 'eliminarProducto' en table.ts; agregamos función 'actualizarProducto' para manejo de cambios en el formulario y las imágenes | modificamos inputs en html

--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -21,6 +21,10 @@ export class TableComponent {
 
   modalVisibleProducto: boolean = false;
 
+  nombreImagen!: string;
+
+  imagen!: string;
+
   //definimos formulario para los productos 
   /**
    * atributos alfanumericos (string) se inicializan con comillas simples
@@ -32,7 +36,7 @@ export class TableComponent {
     precio: new FormControl(0, Validators.required),
     descripcion: new FormControl('', Validators.required),
     categoria: new FormControl('', Validators.required),
-    imagen: new FormControl('', Validators.required),
+    //imagen: new FormControl('', Validators.required),
     alt: new FormControl('', Validators.required)
 
   })
@@ -52,20 +56,65 @@ export class TableComponent {
         precio: this.producto.value.precio!,
         descripcion: this.producto.value.descripcion!,
         categoria: this.producto.value.categoria!,
-        imagen: this.producto.value.imagen!,
+        imagen:"",
         alt: this.producto.value.alt!
 
       }
-      await this.servicioCrud.crearProducto(nuevoProducto)
-        .then(producto => {
-          alert("Ha agregado un nuevo producto con exito!");
+      await this.servicioCrud.subirimagen(this.nombreImagen, this.imagen, "productos")
+      .then(res => {
+        this.servicioCrud.obtenerURLimagen(res)
+        .then(url =>{
+          this.servicioCrud.crearProducto(nuevoProducto, url)
+          .then(producto => {
+            Swal.fire({
+              title:"bien!",
+              text:"ha agregado un nuevo producto con éxito",
+              icon:"success"
+            });
+          })
+          .catch(error => {
+            Swal.fire({
+              title:"oh no!",
+              text:"ha ocurrido un error al cargar un nuevo producto",
+              icon:"error"
+            });
+          })
         })
-        .catch(error => {
-          alert("Ha ocurrido un error al cargar un producto")
-        })
+      } )
+  
+  
     }
 
   }
+
+ //cargar imagenes
+
+ cargarimagen(event: any) {
+  //variable para obtener el archivo subidoo desde el htmml 
+  let archivo = event.target.files[0];
+
+  //variable para crear un nuevo objeto de tipo "archivo" o "file" y leerlo
+  let reader = new FileReader();
+
+
+  if (archivo != undefined) {
+    //llamamos a metodo readeAsdataURL para leer toda la informacion recibida enviamos como parametro el archivo
+    //porque sera el encargador de tener la info ingresada pior el usuario
+    reader.readAsDataURL(archivo);
+
+    reader.onloadend = () => {
+      let url = reader.result;
+
+      if (url!= null) {
+        this.nombreImagen = archivo.name;
+
+
+        this.imagen = url.toString();
+      }
+    }
+  }
+}
+
   
   //
   mostrarBorrar(productoSeleccionado: Producto){
@@ -76,12 +125,24 @@ export class TableComponent {
   }
 
   borrarProducto(){
-    this.servicioCrud.eliminarProducto(this.productoSeleccionado.idProducto)
+    this.servicioCrud.eliminarProducto(this.productoSeleccionado.idProducto, this.productoSeleccionado.imagen)
     .then(respuesta => {
       alert("Se ha podido eliminar con exito!");
     })
     .catch(error => {
       alert ("Ha ocurrido un error al eliminar el producto :( \n"+error);
+    })
+  }
+  mostrarEditar(productoSeleccionado: Producto){
+    this.productoSeleccionado= productoSeleccionado;
+
+    this.producto.setValue({
+      nombre: this.producto.value.nombre!,
+    precio: this.producto.value.precio!,
+    descripcion: this.producto.value.descripcion!,
+    categoria: this.producto.value.categoria!,
+    //imagen:productoSeleccionado.imagen,
+    alt: this.producto.value.alt!,
     })
   }
 
@@ -94,9 +155,39 @@ editarProducto(){
     precio: this.producto.value.precio!,
     descripcion: this.producto.value.descripcion!,
     categoria: this.producto.value.categoria!,
-    imagen: this.producto.value.imagen!,
+    imagen:this.productoSeleccionado.imagen,
     alt: this.producto.value.alt!,
   }
+//vamos a verificar si el usuario  ingresa o no una nueva imagen
+  if(this.imagen){
+    this.servicioCrud.subirimagen(this.nombreImagen, this.imagen, "productos")
+    .then(res => {
+      this.servicioCrud.obtenerURLimagen(res)
+      .then(url =>{
+        datos.imagen = url; //actualizamos la url de la imagen en los datos del formulario
+
+        this.actualizarProducto(datos);//actualizamos los datos
+
+        this.producto.reset();//vaciar las casillas del formlario
+
+
+      })
+      .catch(error => {
+        alert("Hubo un problema al subir la imagen \n"+error);
+        this.producto.reset()
+      })
+    })
+  }else{
+    //actializamos formulario con los datos recibidos del usuario pero sin modificar la imagen que ya existe en firestore y storage
+    this.actualizarProducto(datos);
+  }
+
+ 
+}
+
+//ACTUALIZAR la información ya existente de los productos
+actualizarProducto(datos: Producto){
+  //enviamos al método el id jdel prodcuto seleccionado y los datos actualizads
   this.servicioCrud.modificarProducto(this.productoSeleccionado.idProducto, datos)
   .then(articulo =>{
     Swal.fire({
--- src/app/modules/admin/services/crud.service.ts
+++ src/app/modules/admin/services/crud.service.ts
@@ -5,6 +5,20 @@ import { Producto } from 'src/app/models/producto';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { map } from 'rxjs';
 import { Action } from 'rxjs/internal/scheduler/Action';
+//importaciones para manejjo de archivos y referencias 
+import { getDownloadURL, getStorage, ref, UploadResult, uploadString, deleteObject } from 'firebase/storage';
+
+//la primera importancion es para oobtener la url de descarga de una imagen subida 
+
+//getStorage para obtener la instancia de almacenamiento
+
+//ref para crear referencias a ubicaciones en el almacenamiento
+
+//uploadresult tipo que rerpresennta el resultado de una operacion subida
+
+//uploadstring para sbuir imagen¿es en formate cadena
+
+//deleteobject para eliminar un espacio en el almacenamiento 
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +26,10 @@ import { Action } from 'rxjs/internal/scheduler/Action';
 export class CrudService {
   //definimos coleccion para los productos de la web
   private productoColecction: AngularFirestoreCollection<Producto>
+  //definimos VARIABLE RESPUESTA QUE PODRA SUBIR RESULTADOS
+  private respuesta!: UploadResult;
+  //inicializar servicio de storage 
+  private storage = getStorage();
 
 
 
@@ -21,13 +39,15 @@ export class CrudService {
    }
 
    //crear productos
-   crearProducto(producto: Producto){
+   crearProducto(producto: Producto, url:string){
     return new Promise(async(resolve,reject)=>{
       try{
         //Creamos numero identificativo para el producto en la Bd
         const idProducto = this.database.createId();
         //asignamos ID creado al atributo idProducto de la inhterfaz Producto
         producto.idProducto = idProducto;
+           //asignamos url recibida del parametro al atributo imagen de interfaz producto 
+           producto.imagen = url;
 
         //
         const resultado = await this.productoColecction.doc(idProducto).set(producto);
@@ -59,21 +79,66 @@ export class CrudService {
     return this.database.collection('productos').doc(idProducto).update(nuevaData);
   }
    //eliminar productos
-   eliminarProducto(idProducto: string){
+   eliminarProducto(idProducto: string, imagenUrl : string){
     //
     return new Promise((resolve, reject) => {
       try{
-        const respuesta = this.productoColecction.doc(idProducto).delete();
+        //definimos referencias localmente de storage
+        const storage = getStorage();
+        //obtiene la refrencia desde el almacenamiento de storage 
+        const referenciaImagen = ref(storage, imagenUrl);
         //
-        resolve (respuesta);
+        deleteObject(referenciaImagen)
+        .then((res)=> {
+          const respuesta = this.productoColecction.doc(idProducto).delete();
+          resolve (respuesta);
+        })
+        .catch(error => {
+          reject("Error al eliminar la imagen: \n"+error);
+        })
+                
       }
       //va a encapsular un error o una falla, por lo tanto lo va a rechazar
       catch(error){
 
         reject(error);
       }
-
     })
    }
+     //obtener url de imagenes
+  obtenerURLimagen(respuesta: UploadResult) {
+    //retorna url obtenmido como referencia
+    return getDownloadURL(respuesta.ref);
+  }
 
+  //Subir imagenes con sus referencias
+  /**
+   * 
+   * @param {string} nombre //nombre de la imagen
+   * @param {any} imagen   //tipo de imagenes que se pueden subir 
+   * @param {string} ruta  //ruta de almacenamiento de las imagenes
+   * @return //se retorna lo obtenido 
+   */
+  async subirimagen(nombre: string, imagen: any, ruta: string) {
+    try {
+
+      //crear una imagen de referencia
+      //accede a storage (almacenamiento), ruta (carpeta) / nombre (nombreImagen)
+      let referenciaImagen = ref(this.storage, ruta + '/' + nombre);
+
+      //asignarla a la respuesta la informacion de las imagenes subidas
+      this.respuesta = await uploadString(referenciaImagen, imagen, 'data_url')
+
+        .then(resp => {
+          return resp;
+        })
+      return this.respuesta
+    }
+    catch (error) {
+      console.log(error);
+
+      return this.respuesta
+    }
+
+}
 }
